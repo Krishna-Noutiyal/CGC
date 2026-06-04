590e76ddfd1ea05123b7e46805e0a697668c476d
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2026-06-04 16:51:49 +0530

    chore: Updated `build.ps1` & Installer script
    This commit significantly reducess the build size by remove unnecessary file and folder from the build script.
    
    > Also Improved the Installer and Uninstaller for the software

--- build.ps1
+++ build.ps1
@@ -89,7 +89,7 @@ try {
         --build-version $version `
         --company "Pooja ITR Center" `
         --copyright "Copyright (C) 2025 Pooja ITR Center" `
-        --exclude "release, icons, requirements.txt, README.md, certs, test, .venv" `
+        --exclude ".git, build.ps1, release, icons, CGC-cache, README.md, certs, test, .gitignore, .vscode, .venv, CGC.aip, updates.txt, requirements.txt, README.md, poetry.lock, Dashboards" `
         --clear-cache --compile-app --compile-packages --cleanup-app --cleanup-packages --cleanup-app --module-name .\main.py
 
     if ($LASTEXITCODE -eq 0) {
